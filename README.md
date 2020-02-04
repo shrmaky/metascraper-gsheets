fa29e3e1bac021a0cf1d325cb89d76b3abf9053e
Akshat Sharma <er.sharmaakshat@gmail.com>
2020-02-04 18:19:54 +0400

    feat: pushing the basic package with google sheet support

---
+++ README.md
@@ -0,0 +1,32 @@
+## Introduction
+
+There are specific use cases when you need to scrape the website meta. [Metascraper](https://github.com/microlinkhq/metascraper) is an amazing library which makes this easy. My use case was to scrape the website meta & save it in data, so i thought of why not saving directly in google sheet to make in more friendly to non-tech people as well. 
+
+
+###Step:1 Generate Google auth-
+
+1. Google Developers Console - https://console.developers.google.com/
+2. Create a Project/Select if you already have
+3. Enable the Sheets API for the project
+4. Create Credentials
+5. Create Key -> Json
+6. Download Json
+6. Copy the client_email & private_key & update .env
+
+###Step:2 Setup Instructions -
+1. create an .env file from .env.example
+2. Generate the .env credentials by following the below instructions to generate credentials
+3. Create a google sheet from your account & share your sheet with the client_email generated.
+4. Copy the sheet id - https://docs.google.com/spreadsheets/d/SHEET_ID/
+5. Inside the project folder
+6. npm install
+7. node index.js
+
+
+###Thanks to developers of 
+
+* [Metascraper](https://github.com/microlinkhq/metascraper)
+
+
+
+
---
+++ index.js
@@ -0,0 +1,65 @@
+require('dotenv').config();
+
+const {GoogleSpreadsheet} = require('google-spreadsheet');
+
+const metascraper = require('metascraper')([
+    require('metascraper-author')(),
+    require('metascraper-video')(),
+    require('metascraper-description')(),
+    require('metascraper-image')(),
+    require('metascraper-date')(),
+    require('metascraper-clearbit')({
+        format: 'png'
+    })
+]);
+
+const got = require('got');
+
+//const targetUrl = 'https://www.alibaba.com/product-detail/Food-Beverage-Cosmetics-Natural-Preservative-Nisin_60718821637.html?spm=a2700.8270666-2.201612261929.9.2f306978lj1GzV';
+const targetUrl = 'https://www.google.com';
+
+(async () => {
+    const {body: html, url} = await got(targetUrl);
+    /** @type {object} */
+    const metadata = await metascraper(
+        {
+            html,
+            url
+        });
+
+    //console.log(metadata); You can uncomment this at the time of testing.
+
+
+    // spreadsheet key is the long id in the sheets URL https://docs.google.com/spreadsheets/d/SHEET_ID/
+    const doc = new GoogleSpreadsheet(process.env.SHEET_ID);
+
+    // use service account creds
+    await doc.useServiceAccountAuth({
+        client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
+        private_key: process.env.GOOGLE_PRIVATE_KEY,
+    });
+
+    await doc.loadInfo();
+
+    //const newSheet = await doc.addSheet({ title: 'Test-Sheet' });
+    //const sheet = await doc.addSheet({ headers: ['author', 'image','video','description','date','logo','publisher'] });
+
+    const sheet = doc.sheetsByIndex[0];
+
+    //console.log(sheet.cellStats.loaded);
+    //const rows = await sheet.getRows();
+    //console.log(rows.length);
+
+    // append rows
+    await sheet.addRow({
+        url: url,
+        author: metadata.author,
+        image: metadata.image,
+        video: metadata.video,
+        description: metadata.description,
+        date: metadata.date,
+        logo: metadata.logo,
+        publisher: metadata.publisher
+    });
+    console.log('Yahoo! Sheet updated');
+})();
